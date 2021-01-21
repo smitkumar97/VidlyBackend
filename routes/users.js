90336215459ff116eaadd42b85f962466aaa0918
SMIT PATEL <smitkumar97@gmail.com>
2021-01-21 13:47:48 +0530

    initial commit

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const auth = require("../middleware/auth");
-const bcrypt = require("bcrypt");
+const bcryptjs = require("bcryptjs");
 const _ = require("lodash");
 const { User, validate } = require("../models/user");
 const express = require("express");
@@ -10,21 +10,28 @@ router.get("/me", auth, async (req, res) => {
   res.send(user);
 });
 
+router.get("/", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
+
   if (user) return res.status(400).send("User already registered.");
 
   user = new User(_.pick(req.body, ["name", "email", "password"]));
-  const salt = await bcrypt.genSalt(10);
-  user.password = await bcrypt.hash(user.password, salt);
+  // const salt = await bcryptjs.genSalt(10);
+  const salt = await bcryptjs.genSaltSync(10);
+  user.password = await bcryptjs.hashSync(user.password, salt);
   await user.save();
 
   const token = user.generateAuthToken();
   res
     .header("x-auth-token", token)
+    .header("access-control-expose-headers", "x-auth-token")
     .send(_.pick(user, ["_id", "name", "email"]));
 });
 
